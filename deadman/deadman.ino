856a142385a4f4f70531e3dbcd6d4148885fde07
Billy Wood <Willybood@yahoo.co.uk>
2017-09-22 20:29:15 +0100

    Have added theoretical code for piezzo buzzers.

--- deadman/deadman.ino
+++ deadman/deadman.ino
@@ -1,9 +1,11 @@
 /*
  * When the button is pushed a timer counts down.
  * When it reaches a set amount, it starts buzzing until the button is depressed.
+ * Piezo buzzer code is taken from https://www.arduino.cc/en/Tutorial/ToneMelody?from=Tutorial.Tone
  */
 
 #include "Timer.h"
+#include "pitches.h"
 
 const int BUZZER_PIN = 7; // Output pin for the buzzer/motor
 const int BUTTON_PIN = 8; // Input pin for the button to be held down
@@ -11,8 +13,19 @@ const int RED_LED_PIN = 4; // Output pins for the LEDs
 const int GREEN_LED_PIN = 2; // Output pins for the LEDs
 const int BUTTON_V_PIN = 9; // Input pin for the button, provides the voltage needed
 
-const int SECONDS_TO_WAIT = 90; // The amount of time to wait before
-const int OSCILLATION_TIME = 1000; // The time between motor oscillations
+const int SECONDS_TO_WAIT = 90UL; // The amount of time to wait before setting off the alarm
+const int TIME_BETWEEN_BUZZES = 3000; // The time between tones played
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+const int NUM_OF_NOTES = 8;
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
 
 // Events used by the timer library
 int buzzerEvent; // The event that controls the buzzing
@@ -41,10 +54,29 @@ void setup() {
   Serial.println("Program started");
 }
 
+// Plays the tone
+void playTone() {
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    const int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
+}
+
 // Starts the buzzing
 void startBuzzing() {
   Serial.println(("Entering %s", __FUNCTION__));
-  buzzerEvent = timer.oscillate(BUZZER_PIN, OSCILLATION_TIME, HIGH);
+  buzzerEvent = timer.every(TIME_BETWEEN_BUZZES, playTone);
 }
 
 // Stops the buzzing
@@ -56,7 +88,8 @@ void stopBuzzing() {
 // Starts the countdown
 void startCountdown() {
   Serial.println(("Entering %s", __FUNCTION__));
-  timerEvent = timer.after(SECONDS_TO_WAIT * 1000, startBuzzing);
+  const unsigned long timeToWait = SECONDS_TO_WAIT * 1000UL;
+  timerEvent = timer.after(timeToWait, startBuzzing);
 }
 
 // Stops the countdown
